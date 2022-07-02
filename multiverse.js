76c4e72032b9e86b8037a49104217eb2dbc95c13
Jason Lu <jasonlu6@bu.edu>
2022-07-02 16:28:01 -0400

    Algos in 10 Season 3 Episode 4 coding
    Part A (React), Part B (Kubernetes Job / Cronjob) available. Part C will not be included due to it being part of Gayle's CTCI solution (and that it is a self-explanatory solution to a common interview question for linked lists about palindrome linked lists).

---
+++ multiverse.js
@@ -0,0 +1,8 @@
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(<h1 class="english">Hello, world!</h1>);
+root.render(<h1 class="spanish">Holla mundo!</h1>);
+root.render(<h1 class="chinese">你好世界!</h1>);
+root.render(<App />);
+
